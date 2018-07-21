18efd923211db69ba3d2f119e82280d9556d32c1
Prashant N P <pnp1808@gmail.com>
2018-07-22 00:55:22 +0530

    Delete view spec as feature spec is present for home page

--- spec/views/grid_cells/index.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "grid_cells/index.html.erb", type: :view do
-  it "displays header" do
-    render
-    expect(rendered).to match /Simple Drawing App/
-  end
-end
